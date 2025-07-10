b75eff2b19058f99d5241867d5f8180a1bbaa06f
Nikunjpindaria <editsytmanage@gmail.com>
2025-07-10 08:02:04 +0530

    Add files via upload

---
+++ AdditionArray.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class AdditionArray {
+    
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size of array:");
+        int n=sc.nextInt();
+        System.out.println("enter size of array:");
+        int m=sc.nextInt();
+        int[][] arr=new int[n][m];
+
+
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.println("enter element at position ["+i+"]["+j+"]");
+                arr[i][j]=sc.nextInt();
+
+
+            }
+            
+            
+        }
+       
+        
+        System.out.println("array is:");
+        for(int i=0;i<n;i++){
+             for(int j=0;j<m;j++){
+                
+                System.out.print(arr[i][j]+" ");
+                
+
+
+            }
+            System.out.println();
+        }
+
+    } 
+
+}
